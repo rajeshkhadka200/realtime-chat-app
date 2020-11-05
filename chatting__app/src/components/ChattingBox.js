b72169b98b85c0d6467788f0073a05a47efc2025
saroj-dev <55448895+saroj-dev@users.noreply.github.com>
2020-11-05 20:33:34 +0545

    Clean up and basic component setup is done

---
+++ chatting__app/src/components/ChattingBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./css/ChattingBox.css";
+
+const ChattingBox = () => {
+    return (
+        <div className="main__box">
+            djkljfklj
+        </div>
+    )
+}
+
+export default ChattingBox
