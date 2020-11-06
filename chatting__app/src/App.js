124df54f6ecfd6366ddb9e5702d3963bc67535bd
rajesh khadka <techyrajesh45@gmail.com>
2020-11-06 22:27:39 +0545

    prevent error

--- chatting__app/src/App.js
+++ chatting__app/src/App.js
@@ -1,11 +1,13 @@
 import './css/App.css';
 import ChattingBox from "./components/ChattingBox";
-
+import { BlurOnTwoTone } from '@material-ui/icons';
+ 
 function App() {
   return (
     <div className="main__app__container"> 
     <ChattingBox />
     </div>
+    
   );
 }
 
