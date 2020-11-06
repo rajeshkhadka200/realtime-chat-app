f42acabf788b2aee09fb883db7b5edb26cbf5b36
rajesh khadka <techyrajesh45@gmail.com>
2020-11-06 12:08:30 +0545

    next commit

--- chatting__app/src/components/Header.jsx
+++ chatting__app/src/components/Header.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import '../css/ChattingBox.css';
 import LogoIcon from '../img/logo.jpg'
+ 
 function Header() {
     return (
       <>
       <div className="header">
           <div className="logo">
               <div className="img_container">
-                  <img src={LogoIcon} alt="Not Found"/>
-                  
+                  <img src={LogoIcon} alt="Not Found"/> 
               </div>
               <div className="logo_name">
                   SR Boyz 
               </div>
           </div>
           <div className="active_status">
-              Active Status
-              <span></span>
+              Active People
+              <span>5</span>
           </div>
       </div>
       </>  
