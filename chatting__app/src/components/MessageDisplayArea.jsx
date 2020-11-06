4134758a3a042243fab495a099ee90cd7ed8e0d5
saroj-dev <55448895+saroj-dev@users.noreply.github.com>
2020-11-06 11:23:30 +0545

    made the chatting template

--- chatting__app/src/components/MessageDisplayArea.jsx
+++ chatting__app/src/components/MessageDisplayArea.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const MessageDisplayArea = () => {
-    return (
-        <div className="messageArea">
-            <div className="user1">
-                <div className="message__row">
-                    <div className="message">
-                        Hello
-                    </div>
-                </div>
-                <div className="message__row">
-                    <div className="message">
-                        Hi 
-                    </div>
-                </div>
-                <div className="message__row">
-                    <div className="message">
-                        How are you
-                    </div>
-                </div>
-                <div className="message__row">
-                    <div className="message">
-                        Me too.
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="messageArea">
+      <div className="user1">
+        <img className="user__logo"src="https://avatars2.githubusercontent.com/u/55448895?s=400&u=7dc0dcebbea6d8a1e5b3b088fa4678de3064ff98&v=4" />
+       <div className="message__container">
+       <div className="message__row">
+          <div className="message">Hello</div>
         </div>
-    )
-}
+        <div className="message__row">
+          <div className="message">Hi</div>
+        </div>
+        <div className="message__row">
+          <div className="message">How are you</div>
+        </div>
+        <div className="message__row">
+          <div className="message">Me too.</div>
+        </div>
+       </div>
+      </div>
 
-export default MessageDisplayArea
+
+      <div className="user2">
+       <div className="message__container">
+       <div className="message__row">
+          <div className="message">Hello</div>
+        </div>
+        <div className="message__row">
+          <div className="message">Hi</div>
+        </div>
+        <div className="message__row">
+          <div className="message">How are you</div>
+        </div>
+        <div className="message__row">
+          <div className="message">Me too.</div>
+        </div>
+       </div>
+      <img className="user__logo"src="https://avatars2.githubusercontent.com/u/55448895?s=400&u=7dc0dcebbea6d8a1e5b3b088fa4678de3064ff98&v=4" />
+      </div>
+    </div>
+  );
+};
+
+export default MessageDisplayArea;
